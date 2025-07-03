468f2c5df4c0c2c26416a0326792005efa1403a6
Sergio del Amo <sergio.delamo@softamo.com>
2025-07-03 09:22:15 +0200

    use projectgen-bom 0.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("io.micronaut.sourcegen:micronaut-sourcegen-annotations")
 
     // ProjectGen
-    implementation(platform("io.micronaut.projectgen:micronaut-projectgen-bom:0.0.1"))
+    implementation(platform("io.micronaut.projectgen:micronaut-projectgen-bom:0.0.2"))
     implementation("io.micronaut.projectgen:micronaut-projectgen-http-server")
     testImplementation("io.micronaut.projectgen:micronaut-projectgen-test")
 
